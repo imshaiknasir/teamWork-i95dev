7e3af306daa78bfa1fb54a81ee570bb1b8bf4b28
imshaiknasir <skfiroj1994@gmail.com>
2020-02-11 23:21:51 +0530

    Created proj.

---
+++ src/test/java/com/i95dev/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.i95dev.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
